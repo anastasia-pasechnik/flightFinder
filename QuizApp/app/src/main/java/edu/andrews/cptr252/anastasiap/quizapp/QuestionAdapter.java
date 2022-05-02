5f2128bfbad55cc4474b2db1165f5762b6774216
FireCat16 <thefirecat16@gmail.com>
2022-05-02 00:46:22 -0400

    Displaying list of fake questions

---
+++ QuizApp/app/src/main/java/edu/andrews/cptr252/anastasiap/quizapp/QuestionAdapter.java
@@ -0,0 +1,114 @@
+package edu.andrews.cptr252.anastasiap.quizapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHolder>{
+
+    private static final String TAG="QuestionAdapter";
+    /** Used to store reference to list of questions*/
+    private ArrayList<Question> mQuestions;
+    /**
+     * Constructor for BugAdapter. Initialize adapter with given list of questions.
+     * @param questions list of questions to display.
+     */
+    public QuestionAdapter(ArrayList<Question> questions) {
+        mQuestions = questions;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        /** TextView that displays question title */
+        public TextView questionTitleTextView;
+        /** TextView that displays question answer */
+        public TextView questionAnswerTextView;
+
+
+        /** Create a new view holder for a given view item in the bug list */
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // Store references to the widgets on the view item
+            questionTitleTextView = itemView.findViewById(R.id.question_list_item_questionTextView);
+
+            questionAnswerTextView = itemView.findViewById(R.id.question_list_item_answerTextView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        /**
+         * OnClick listener for question in the question list.
+         * Triggered when user clicks on a question in the list
+         * @param v is view for question that was clicked
+         */
+
+        @Override
+        public void onClick(View v) {
+            // Get index of question that was clicked.
+            int position = getAdapterPosition();
+            // For now, just display the question title.
+            // In the future, open the selected question.
+            if (position != RecyclerView.NO_POSITION) {
+                Question question = mQuestions.get(position);
+                Log.d(TAG, question.getTitle() + " was clicked");
+            }
+        }
+    } // end ViewHolder
+
+
+    /**
+     * Create a new view to display a question.
+     * Return the ViewHolder that stores references to the widgets on the new view.
+     */
+    @Override
+    public QuestionAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Get the layout inflater from parent that contains the question view item
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // Inflate the layout to display a question in the list
+        View bugView = inflater.inflate(R.layout.list_item_question, parent, false);
+        // Create a view holder to store references to the widgets on the new view.
+        ViewHolder viewHolder = new ViewHolder(bugView);
+
+        return viewHolder;
+    }
+
+    /**
+     * Display given bug in the view referenced by the given ViewHolder.
+     * @param viewHolder Contains references to widgets used to display bug.
+     * @param position Index of the bug in the list
+     */
+
+    @Override
+    public void onBindViewHolder(QuestionAdapter.ViewHolder viewHolder, int position) {
+        // Get question for given index in question list
+        Question question = mQuestions.get(position);
+        // Get references to widgets stored in the ViewHolder
+        TextView questionTitleTextView = viewHolder.questionTitleTextView;
+
+        TextView questionAnswerTextView = viewHolder.questionAnswerTextView;
+
+        // Update widgets on view with question details
+        questionTitleTextView.setText(question.getTitle());
+
+        questionAnswerTextView.setText(question.getTitle());
+
+    }
+
+    /**
+     * Get number of questions in list.
+     */
+    @Override
+    public int getItemCount() {
+        return mQuestions.size();
+    }
+
+
+} // end of QuestionAdapter class
--- QuizApp/app/src/main/java/edu/andrews/cptr252/anastasiap/quizapp/QuestionListFragment.java
+++ QuizApp/app/src/main/java/edu/andrews/cptr252/anastasiap/quizapp/QuestionListFragment.java
@@ -7,10 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class QuestionListFragment extends Fragment {
+
+    /** RecyclerView that displays list of questions */
+    private RecyclerView mRecyclerView;
+    /** Adapter that generates/resuses views to display questions */
+    private QuestionAdapter mQuestionAdapter;
 
     /** Tag for message log */
     private static final String TAG = "QuestionListFragment";
@@ -25,9 +32,15 @@ public class QuestionListFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.question_list_label);
+
         mQuestions = QuestionList.getInstance(getActivity()).getQuestions();
+
+        // use our custom bug adapter for generating views for each bug
+        mQuestionAdapter = new QuestionAdapter(mQuestions);
+
         // for now, list bugs in log
         for (Question question: mQuestions){
             Log.d(TAG, question.getTitle());
@@ -39,6 +52,14 @@ public class QuestionListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_question_list, container, false);
+
+        mRecyclerView = v.findViewById(R.id.question_list_recyclerView);
+        // RecyclerView will use the QuestionAdapter to create views for questions
+        mRecyclerView.setAdapter(mQuestionAdapter);
+        // Use a linear layout when displaying questions
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
 
         return v;
 
